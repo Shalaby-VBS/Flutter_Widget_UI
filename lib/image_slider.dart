9d41331fc8988c544f6da1fa2930a2043f38b335
Shalaby-VBS <shalaby.vbs@gmail.com>
2024-03-16 14:44:27 +0200

    Shalaby-VBS

--- lib/image_slider.dart
+++ lib/image_slider.dart
@@ -14,6 +14,7 @@ class _HomeState extends State<ImageSliders> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("ImageSlider in Flutter"),
+        // backgroundColor: Colors.blue[200],
         centerTitle: true,
       ),
       body: Column(
